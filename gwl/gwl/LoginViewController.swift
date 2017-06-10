c35e04a6df46e73e717a07b67eff651723035b36
Josh Marketis <josh@clidus.com>
2017-06-10 15:41:14 +0100

    Save password to KeyChain

--- gwl/gwl/LoginViewController.swift
+++ gwl/gwl/LoginViewController.swift
@@ -30,6 +30,11 @@ class LoginViewController: UIViewController, LoginDelegate {
     
     func didPressLoginButton(username: String, password: String) {
         let loginModel = LoginModel()
+        
+        if(!savePasswordToKeyChain(password: password)) {
+            print("Failed to save password to KeyChain")
+        }
+        
         loginModel.login(username: username, password: password)
         
         // remove self
@@ -37,6 +42,26 @@ class LoginViewController: UIViewController, LoginDelegate {
         
         // display navigation
         //appDelegate.navController.setNavigationBarHidden(false, animated: false)
+    }
+    
+    func savePasswordToKeyChain(password: String) -> Bool {
+        // save password to keychain so we can retrieve it
+        // when session token expires to re-authenticate
+        let status = KeyChainModel.save(key: "Password", data: Data(from: password))
+        
+        if(status == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    func getPasswordFromKeyChain() -> String? {
+        if let receivedData = KeyChainModel.load(key: "Password") {
+            return receivedData.to(type: String.self)
+        }
+        
+        return nil;
     }
 }
 
